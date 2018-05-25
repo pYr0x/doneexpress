5ea29473a5b896e39e327e8ff7fcf2f391fcac99
Julian Kern <kern.julian@gmx.de>
2018-05-25 20:42:32 +0200

    add route

--- public/app.js
+++ public/app.js
@@ -1,7 +1,16 @@
 import DefineMap from "can-define/map/map";
+import route from 'can-route';
+import "can-route-pushstate";
 
 const AppViewModel = DefineMap.extend('App', {
-
+  /**
+   * @property {String} bitballs/app.page page
+   * @parent bitballs/app.properties
+   * Current page
+   **/
+  page: 'string'
 });
+
+route.register('{page}',{page: 'tournaments'});
 
 export default AppViewModel;
